ff3f02402ea43f90d7c9bdc63e533d19a50e88f9
Rafael Stein Matsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2018-04-06 13:56:22 -0300

    Add files via upload

---
+++ programa09_03_04.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <locale.h>
+int main (){
+    int i;
+    int j;
+    int numero;
+    setlocale (LC_ALL, "");
+    printf ("Informe o número: ");
+    scanf ("%d", &numero);
+    for (i = 1; i <= numero; i++){
+         for (j = 1; j <= i; j++){
+             printf ("*");
+         }
+    printf ("\n");        
+    }
+    system ("pause");
+    return 0;
+}
