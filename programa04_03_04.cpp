f564a778b86588d1a806fac4cada054cda3daf6a
Rafael Stein Matsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2018-04-08 16:16:47 -0300

    Add files via upload

---
+++ programa04_03_04.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <locale.h>
+int main (){
+    int i;
+    int j;
+    int tamanhoPalavra;
+    int tamanhoFrase;
+    int tamanhoComparacao;
+    char frase[256];
+    char palavra[256];
+    setlocale (LC_ALL, "");
+    printf ("Informe a frase: ");
+    gets (frase);
+    strlwr(frase);
+    printf ("\n");
+    printf ("Informe a palavra: ");
+    gets (palavra);
+    strlwr(palavra);
+    printf ("\n");
+    tamanhoFrase = strlen(frase);
+    tamanhoPalavra = strlen(palavra);
+    tamanhoComparacao = 0;
+    for ( i = 0; i <  tamanhoFrase; i++){
+        for (j = 0; j <  tamanhoPalavra; j++){
+            if (frase[i] == palavra[j]){
+               tamanhoComparacao++;
+            } 
+			if (frase[i] == 32){
+				if (tamanhoComparacao == tamanhoPalavra){
+        			printf ("A palavra %s está contida na frase. \n", palavra);
+        			break;
+    			} 
+    			// repete o looping, atribuindo 0 à tamanhoComparacao
+    			else{
+    				tamanhoComparacao = 0;
+    			}
+			}       
+        }
+    }
+    printf ("Não está\n");
+    system ("pause");
+    return 0;                    
+}   
