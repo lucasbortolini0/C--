33741ee629569f0203b4abe3de39960aca2c21aa
Rafael Stein Matsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2018-04-16 19:42:40 -0300

    Update programa09_03_04.cpp

--- programa09_03_04.cpp
+++ programa09_03_04.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <locale.h>
 int main (){
     int i;
     int j;
     int numero;
     setlocale (LC_ALL, "");
-    printf ("Informe o número: ");
+    printf ("Informe o nÃºmero: ");
     scanf ("%d", &numero);
     for (i = 1; i <= numero; i++){
          for (j = 1; j <= i; j++){
