98d7993563e4915766afbedd40103793dd0f6dc2
Rafael Stein Matsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2018-04-16 19:40:23 -0300

    Update programa12_03_04.cpp

--- programa12_03_04.cpp
+++ programa12_03_04.cpp
@@ -4,13 +4,13 @@ int main (int argc, char *argv[]){
     int i;
     int j;
     // vetor
-    int resultado[10];
-    // define o número a ser multiplicado
-    for (i = 1; i <= 10; i++){
-        // realiza de fato o processo de multiplicação
-        for (j = 1; j <= 10; j++){
-            resultado [j] = i * j;
-            printf ("%d * %d = %d\n", i, j, resultado[j]);
+    int resultado[11];
+    // define o nÃºmero a ser multiplicado
+    for (i = 1; i < 11; i++){
+        // realiza de fato o processo de multiplicaÃ§Ã£o
+        for (j = 1; j < 11; j++){
+            resultado[i] = i * j;
+            printf ("%d * %d = %d\n", i, j, resultado[i]);
         }
     printf ("\n");
     }
