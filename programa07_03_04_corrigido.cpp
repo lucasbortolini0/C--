11d21e99ce5602f35380be47c4fce8394b0f1153
Rafael Stein Matsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2018-04-16 20:48:37 -0300

    Update programa07_03_04_corrigido.cpp

--- programa07_03_04_corrigido.cpp
+++ programa07_03_04_corrigido.cpp
@@ -3,33 +3,30 @@
 #include <string.h>
 #include <ctype.h>
 #include <locale.h>
-int main (){
+int main (int argc, char*argv[]){
     int i;
     int numero1;
     int numero2;
+    int numeroInicio;
     int soma;
     setlocale (LC_ALL, "");
-    printf ("Informe o primeiro número: ");
+    printf ("Informe o primeiro nÃºmero: ");
     scanf ("%d", &numero1);
     printf ("\n");
-    printf ("Informe o segundo número: ");
+    printf ("Informe o segundo nÃºmero: ");
     scanf ("%d", &numero2);
     printf ("\n");
-    if (numero2 > numero1){
-       soma = 0;
-       /* o comando a seguir vai atribuindo +1 ao primeiro número, somando com o 
-       anterior e por fim acrescentando numa variável soma */
-       for (i = numero1; i <= numero2; i++){
-           soma = numero1 + soma;
-           numero1++;
-       }
-       printf ("Primeiro número: %d\n", numero1);
-       printf ("Segundo número: %d\n", numero2);
-       printf ("Soma: %d\n", soma);
+    numeroInicio = numero1;
+    soma = 0;
+    /* o comando a seguir vai atribuindo +1 ao primeiro nÃºmero, somando com o 
+       anterior e por fim acrescentando numa variÃ¡vel soma */
+    for (i = numero1; i <= numero2; i++){
+         soma = numeroInicio + soma;
+         numeroInicio++;
     }
-    else {
-         printf ("Os números não são válidos\n");
-    }     
+    printf ("Primeiro nÃºmero: %d\n", numero1);
+    printf ("Segundo nÃºmero: %d\n", numero2);
+    printf ("Soma: %d\n", soma);
     system ("pause");
     return 0;
 }
