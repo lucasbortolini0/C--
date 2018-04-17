bd7a769fde3f359dc6c9367ff48fcd02c6288744
Rafael Stein Matsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2018-04-16 21:16:31 -0300

    Update programa08_03_04.cpp

--- programa08_03_04.cpp
+++ programa08_03_04.cpp
@@ -3,27 +3,24 @@
 #include <string.h>
 #include <ctype.h>
 #include <locale.h>
-int main (){
+int main (int argc, char*argv[]){
     int i;
     int j;
     // vetor 
     int numero[10];
     int soma;
     setlocale (LC_ALL, "");
-    // este comando aloca um número em cada variável do vetor numero[10]
+    // este comando aloca um nÃºmero em cada variÃ¡vel do vetor numero[10]
     for ( i = 1; i <= 10; ){
-        printf ("Informe um número: ");
+        printf ("Informe um nÃºmero: ");
         scanf ("%d", &numero[i]);
         i++;
         printf ("\n");
     }
-    soma = 0;
-    // este comando faz a somatória
-    for ( j = 1; j <= 10;){
-        soma = soma + numero[j];
-        j++;
-    }
+    soma = numero[1] + numero[2] + numero[3] +
+    numero[4] + numero[5] + numero[6] + numero[7] +
+    numero[8] + numero[9] + numero[10]; 
     printf ("Soma: %d\n", soma);
     system ("pause");
     return 0;
-}      
+}    
