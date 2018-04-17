9f2291785d09b375e68aafceadb949d6b8659442
Rafael Stein Matsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2018-04-17 19:46:25 -0300

    Update programa04corrigido_03_04.cpp

--- programa04corrigido_03_04.cpp
+++ programa04corrigido_03_04.cpp
@@ -1,50 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include <string.h>
-#include <ctype.h>
 #include <locale.h>
-#define TRUE 1
-#define FALSE 0
-int main (int argc, char*argv[]){
-    int i;
-    int j;
-    int achou = FALSE;
-    int tamanhoPalavra;
-    int tamanhoFrase;
-    int tamanhoComparacao;
-    char frase[256];
-    char palavra[256];
-    setlocale (LC_ALL, "");
-    printf ("Informe a frase: ");
-    gets (frase);
-    strlwr(frase);
-    printf ("\n");
-    printf ("Informe a palavra: ");
-    gets (palavra);
-    strlwr(palavra);
-    printf ("\n");
-    tamanhoFrase = strlen(frase);
-    tamanhoPalavra = strlen(palavra);
-    tamanhoComparacao = 0;
-    for ( i = 0; achou == FALSE && i < tamanhoFrase; i++){
-        for (j = 0; achou == FALSE && j < tamanhoPalavra; j++){
-            if (frase[i] == palavra[j]){
-               tamanhoComparacao++;
-               if (tamanhoComparacao == tamanhoPalavra){
-               	  achou = TRUE;	
-               }
-        	}
-		}
+int main(int argc, char*argv[]){
+	char palavra[100];
+	char frase[100];
+	setlocale (LC_ALL, "");
+	printf("\Informe uma frase: ");
+	gets(frase);
+	printf("\n");
+	printf("Informe uma palavra: ");
+	gets(palavra);
+	printf ("\n");
+	if(strstr(frase, palavra) != NULL){
+		printf ("\n");
+  		printf("Existe");
 	}
-	if(achou == TRUE)
-		{
-	    	printf("\n  Está contida \n");
-        } else {
-    		printf("\n Não está contida \n");
-        }
-		system("pause");
-	   	return 0 ;
-	}                 
+	else {
+		printf ("\n");
+		printf("Não existe");
+	}
+	printf ("\n");
+	system ("pause");
+	return 0;
 }
 
 
